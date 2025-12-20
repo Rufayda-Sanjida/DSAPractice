3274f33f98e9a40469d3334a5856f54f5925e957
Rufayda Sanjida <rufaydasanjida1@gmail.com>
2025-12-19 23:05:30 -0500

    Two pointers learn pattern

--- DS/One Pass + Track Current + Track Max/practice/patternStretchers/2stateProb/PS2.py
+++ DS/One Pass + Track Current + Track Max/practice/patternStretchers/2stateProb/PS2.py
@@ -5,36 +5,69 @@ longest + subarray = one pass find current and compare to max
 no care about future only about what has happened so far at that index  
 '''
 
-nums = [-1,2]
+# nums = [-1,2]
 
-maxPosProductLength = 0
-posLength = 0
-negLength = 0
-negProduct = 1
+# maxPosProductLength = 0
+# posLength = 0
+# negLength = 0
+# negProduct = 1
+
+# for x in nums:
+    
+#     if x == 0:
+#         posLength = 0
+#         negLength = 0
+#         negProduct = 1
+#         continue
+    
+#     negProduct = negProduct * x
+    
+#     if x > 0:
+#         posLength = posLength + 1
+#     else:
+#         posLength = 0
+    
+#     negLength = negLength + 1
+
+#     if negProduct > 0:
+#         if maxPosProductLength < negLength:
+#             maxPosProductLength = negLength
+    
+#         if maxPosProductLength < posLength:
+#             maxPosProductLength = posLength
+
+
+# print(maxPosProductLength)
+
+
+nums = [0, 1, -2, -3, -4]
+
+posLen = 0
+negLen = 0
+best = 0
 
 for x in nums:
-    
     if x == 0:
-        posLength = 0
-        negLength = 0
-        negProduct = 1
-        continue
+        posLen = 0
+        negLen = 0
     
-    negProduct = negProduct * x
+    elif x > 0:
+        posLen += 1
+        
+        if negLen > 0:
+            negLen = negLen + 1
+        else: 
+            negLen = 0
     
-    if x > 0:
-        posLength = posLength + 1
-    else:
-        posLength = 0
-    
-    negLength = negLength + 1
+    else:  # x < 0
+        if negLen > 0:   
+            newPos = negLen + 1 
+        else:
+            newPos = 0
+        newNeg = posLen + 1
+        posLen = newPos
+        negLen = newNeg
 
-    if negProduct > 0:
-        if maxPosProductLength < negLength:
-            maxPosProductLength = negLength
-    
-        if maxPosProductLength < posLength:
-            maxPosProductLength = posLength
+    best = max(best, posLen)
 
-
-print(maxPosProductLength)
+print(best)
